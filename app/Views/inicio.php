4db1b3f88092a64cf0ae8d9fa9f1ded6350cf278
Iverson-Vargas <iversonvargas8@gmail.com>
2025-06-22 03:44:49 -0400

    3:44 de la madrugada ¡quiero dormir!

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Se</title>
+    <title>Sistema de Reportes</title>
 </head>
 <body>
     <h1>Pagina principal</h1>
